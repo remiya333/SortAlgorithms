1b2848ffabd2739346e7e58d4c7f6182e27279f5
Remiya <remiya333@gmail.com>
2020-05-18 20:33:07 +0530

    Initial commit

---
+++ BubbleSort.py
@@ -0,0 +1,16 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+
+        for j in range(0, n-i-1):
+
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return (arr)
+
+myList = [12,76,13,98,34,62]
+myList = bubbleSort(myList)
+print(myList)
+
